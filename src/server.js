631aedb0491e79e3cdf8df86e9427316331a32b8
bkhazaal <thajukez101@gmail.com>
2024-05-07 20:45:10 -0700

    kewlllll

--- src/server.js
+++ src/server.js
@@ -4,9 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('src'))
 
-app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/weather.html'));
